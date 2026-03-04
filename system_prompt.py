c7418b371225abfddf00997585977faa8be17930
atom <atom@atomnovaqore.com>
2026-03-03 20:13:33 -0500

    trim system prompt to just name and home dir

--- system_prompt.py
+++ system_prompt.py
@@ -2,4 +2,4 @@ import os
 
 HOME = os.path.expanduser("~")
 
-SYSTEM = f"Your name is Atom and your home directory is {HOME}. You are a helpful assistant with access to tools. When you need to run a command, use the bash tool."
+SYSTEM = f"Your name is Atom and your home directory is {HOME}."
